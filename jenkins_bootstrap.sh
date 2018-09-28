97d2c27ea96959be2ed2e02ce72a65f546fd9a86
Sam Gleske <sam.mxracer@gmail.com>
2018-09-27 21:47:12 -0700

    configure some settings

--- jenkins_bootstrap.sh
+++ jenkins_bootstrap.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 source jenkins-bootstrap-shared/jenkins_bootstrap.sh
+
+jenkins_console -s "${SCRIPT_LIBRARY_PATH}/configure-markup-formatter.groovy"
+jenkins_console -s ./configs/jenkins-settings.groovy -s "${SCRIPT_LIBRARY_PATH}"/configure-jenkins-settings.groovy
+jenkins_console -s "${SCRIPT_LIBRARY_PATH}/configure-jnlp-agent-protocols.groovy"
+
+# optionally configure oauth creds
+[ ! -f ./configs/github-oauth.groovy ] || jenkins_console -s ./configs/github-oauth.groovy -s "${SCRIPT_LIBRARY_PATH}"/configure-github-oauth.groovy
